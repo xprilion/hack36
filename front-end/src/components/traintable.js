e10aca4f381f24128b64a08c04a59c00c8112b51
abhishek71994 <abhishek71994@gmail.com>
2018-01-27 17:02:56 +0530

    table component v2

--- front-end/src/components/traintable.js
+++ front-end/src/components/traintable.js
@@ -12,12 +12,23 @@ class traintable extends Component {
 							<TableHeaderColumn>Day</TableHeaderColumn>
 							<TableHeaderColumn>Sch Dep</TableHeaderColumn>
 							<TableHeaderColumn>ETA/ATA</TableHeaderColumn>
-							<TableHeaderColumn>Delay</TableHeaderColumn>
 							<TableHeaderColumn>ETD/ATD</TableHeaderColumn>
 							<TableHeaderColumn>Delay</TableHeaderColumn>
 						</TableRow>
 					</TableHeader>
 					<TableBody>
+						{this.props.data.route.map(res=>{
+							return(
+								<TableRow>
+									<TableRowColumn>{res.station.name}</TableRowColumn>
+									<TableRowColumn>{res.day}</TableRowColumn>
+									<TableRowColumn>{res.schdep}</TableRowColumn>
+									<TableRowColumn>{res.actarr}</TableRowColumn>
+									<TableRowColumn>{res.actdep}</TableRowColumn>
+									<TableRowColumn>{res.status}</TableRowColumn>
+								</TableRow>
+								)
+						})}
 						<TableRow>
 							<TableRowColumn>1</TableRowColumn>
 							<TableRowColumn>2</TableRowColumn>
